a5b8564b641a8aae071a7ae393442559ffd0b387
Evgeny Reish <e1000057r@gmail.com>
2026-09-24 23:54:55 +0300

    std

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <cstdlib>
-using namespace std;
-
-int main() 
-{ 
-    cout << "Hello, world!" << endl;
-    system("pause");
-    return 0; 
+  
+int main()
+{
+    setlocale(LC_ALL, "");
+    std::wcout << L"привет мир!" << std::endl;
+    std::cout << "hello, world" << std::endl;
 }
